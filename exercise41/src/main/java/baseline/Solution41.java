fd7f5c6e16330ece358e3a8cbf63b9829d4ddcaf
MohitBallikar <mohitballikar@gmail.com>
2021-10-15 18:41:47 -0400

    pseudo and some skeleton

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+import java.io.File;
+
+public class Solution41 {
+
+    public static void main(String[] args)
+    {
+        namesorter();
+    }
+
+    public static void namesorter()
+    {
+        int numofnames = 0;
+
+        File inputFile = new File("exercise41_input.txt");
+
+        
+
+    }
+
+
+
+    /*
+    Create a program that reads in the following list of names from a file called
+    `exercise41_input.txt` and sorts the list alphabetically
+
+    Reads from a file, parses data, sorts data
+        parsing will have to be done through a loop
+        use maps and strings to represent the values for sorting/storing
+    Print out the data with the proper formatting
+     */
+}
