a39e704ee520a2e0152efbd7642c43229fa3b18b
MohitBallikar <mohitballikar@gmail.com>
2021-10-16 18:16:47 -0400

    pseudo and skeleton

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args) {
+        wordfinder();
+
+    }
+
+     static void wordfinder() {
+    }
+}
+
+/*
+Given an input file named `exercise45_input.txt`, read the file and look for all occurrences of the word utilize.
+Replace each occurrence with use.
+Write the modified file to a new file.
+Prompt for the name of the output file.
+Write the output to a new file.
+
+    Will need to use file I/O to parse and create a new file in the data directory
+       BufferedReader(new FileReader("exercise45_input.txt"));
+       FileWriter("exercise45_output.txt", true);
+       Similar to past exercises
+ */
