2a89fa0710e9bddfd29442dec6c924c1513fd65d
MohitBallikar <mohitballikar@gmail.com>
2021-10-15 21:02:48 -0400

    code/comments, will add UML and tests later

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -3,35 +3,127 @@
  *  Copyright 2021 Mohit Ballikar
  */
 package baseline;
-
-import java.io.File;
+//import necessary libraries
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Solution41 {
+        //define the main function with exception handling
+        public static void main(String[] args) throws IOException
+        {
 
-    public static void main(String[] args)
-    {
-        namesorter();
-    }
+            // Call the Name Sorter function/method.
 
-    public static void namesorter()
-    {
-        int numofnames = 0;
+            namesorter();
+        }
 
-        File inputFile = new File("exercise41_input.txt");
+        static void namesorter() throws IOException
+        {
+            //initialize a counter for the names
+            int numofnames =0;
+            try
+            {
+                //Open an output file for writing.
+                FileWriter f = new FileWriter("exercise41_output1.txt", true);
+                BufferedWriter b = new BufferedWriter(f);
+                PrintWriter pw = new PrintWriter(b);
 
-        
-
-    }
 
 
+                //Read the input file.
+                File inputFile = new File("exercise41_input.txt");
+                Scanner inputReader = new Scanner(inputFile);
 
-    /*
-    Create a program that reads in the following list of names from a file called
-    `exercise41_input.txt` and sorts the list alphabetically
+                // 2 Maps for Firstname and lastname
 
-    Reads from a file, parses data, sorts data
-        parsing will have to be done through a loop
-        use maps and strings to represent the values for sorting/storing
-    Print out the data with the proper formatting
-     */
+                Map<Integer, String> firstnamemap = new HashMap<>();
+                Map<Integer, String> lastnamemap = new HashMap<>();
+                int i = 0;
+                //loop appends the data to the maps
+                while (inputReader.hasNextLine())
+                {
+                    String data = inputReader.nextLine();
+                    numofnames++;
+                    i++;
+
+
+                    //Split the names into 2 Maps.
+                    String[] namedata = data.split(",", 2);
+                    firstnamemap.put(i,namedata[1]);
+                    lastnamemap.put(i,namedata[0]);
+
+                }
+
+                //a key is defined for separation between the first and last name
+                String tempkey = "";
+                int key;
+
+                //Now Sort the lastname Map and retrieve the values for the corresponding firstnames.
+                Map<Integer, String> sortedlastname = lastnamemap.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
+
+                //define a string for the names and split it to follow formatting
+                String[] sortedstring = sortedlastname.toString().split(",");
+
+                pw.println("Total of "+ numofnames+" names");
+                pw.println("-----------------");
+                //print out the required header formatted
+                System.out.println("Total of "+ numofnames+" names");
+                System.out.println("-----------------");
+                //loop through the maps to provide the values for the printing
+                for (String s : sortedstring) {
+                    String[] tempsortedstring = s.split("=");
+                    for (int j = 0; j < tempsortedstring.length; j++) {
+                        tempkey = tempsortedstring[j];
+
+                        if (j == 0) {
+                            tempkey = tempkey.substring(1, tempkey.length());
+                            try {
+                                key = Integer.parseInt(tempkey);
+                                if (key > 0) {
+                                    String lastnamevalue = lastnamemap.get(Integer.parseInt(tempkey));
+                                    String firstnamevalue = firstnamemap.get(Integer.parseInt(tempkey));
+                                    String fullname = lastnamevalue + "," + firstnamevalue;
+                                    System.out.printf("%-20s%n", fullname);
+                                    String stringformat = "%1$s\n";
+                                    pw.format(stringformat, fullname);
+                                }
+                            } catch (NumberFormatException e) {
+                                //Do Nothing
+                            }
+                        } else {
+                            //defined twice to ensure data is properly represented, if not correct the first time
+                            try {
+                                key = Integer.parseInt(tempkey);
+                                if (key > 0) {
+                                    String lastnamevalue = lastnamemap.get(Integer.parseInt(tempkey));
+                                    String firstnamevalue = firstnamemap.get(Integer.parseInt(tempkey));
+                                    String fullname = lastnamevalue + "," + firstnamevalue;
+                                    System.out.printf("%-20s%n", fullname);
+                                    String stringformat = "%1$s\n";
+                                    pw.format(stringformat, fullname);
+                                }
+                            } catch (NumberFormatException e) {
+                                //Do Nothing
+                            }
+                        }
+                    }
+                }
+
+                inputReader.close();
+                pw.flush();
+            }
+            //error handling for  I/O issues
+            catch (FileNotFoundException e)
+            {
+                System.out.println("Error Opening File.");
+                e.printStackTrace();
+            }
+
+        }
+
+
 }
