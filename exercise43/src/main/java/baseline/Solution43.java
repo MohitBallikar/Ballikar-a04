a970cecaa3a3588519bbec3808255907a8868898
MohitBallikar <mohitballikar@gmail.com>
2021-10-16 11:07:36 -0400

    psuedo and skeleton

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileOutputStream;
+
+public class Solution43 {
+    public static void main(String[] args) {
+        websitegenerator();
+    }
+
+     static void websitegenerator() {
+         String sitename = "";
+         String author = "";
+         String yesfolder ="";
+         String yescss ="";
+         //store the responses for comparison to the desired input/output
+         //will have to the mkdir called for the sections of code that the user responds yes to
+
+    }
+}
+
+/*
+Create a program that generates a website skeleton with the following specifications:
+
+Prompt for the name of the site.
+Prompt for the author of the site.
+Ask if the user wants a folder for JavaScript files.
+Ask if the user wants a folder for CSS files.
+Generate an index.html file that contains the name of the site inside the <title> tag and the author in a <meta> tag.
+
+    Essentially prompt the user multiple times, generating file/folders whenever the user dictates
+
+
+ */
