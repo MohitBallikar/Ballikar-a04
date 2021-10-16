e07a956b165fca9f2deb87b6dde8f150c6f7f64d
MohitBallikar <mohitballikar@gmail.com>
2021-10-15 21:40:14 -0400

    pseudo/skeleton

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution42 {
+    public static void main(String[] args) {
+        parseDataFile();
+    }
+
+     static void parseDataFile() {
+    }
+}
+
+/*
+Construct a program that reads in the following data file
+(you will need to create this data file yourself;
+name it `exercise42_input.txt`)
+Process the records and display the results formatted as a table,
+evenly spaced, as shown in the example output.
+    Write your own code to parse the data. Don't use a CSV parser.
+
+    Similar I/O structure as 41 could be used to read the data, and possibly even print/format
+    Data must be parsed manually
+    Make sure that the printing follows the necessary requirements for formatting
+ */
