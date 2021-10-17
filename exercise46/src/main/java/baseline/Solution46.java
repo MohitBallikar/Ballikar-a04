d0348c3cab993781bc8b4c463de1e58ff4f8c4bc
MohitBallikar <mohitballikar@gmail.com>
2021-10-17 12:47:01 -0400

    pseudo and skeleton

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Solution46 {
+    public static void main(String[] args) {
+        wordfrequencyfinder();
+    }
+
+    static void wordfrequencyfinder() {
+    }
+}
+
+/*
+Create a program that reads in a file named `exercise46_input.txt`
+and counts the frequency of words in the file.
+Then construct a histogram displaying the words and the frequency,
+and display the histogram to the screen.
+
+    Ensure that the most used word is at the top of the report and the least used words are at the bottom.
+
+    Will most definitely use loops to check for each term, as well as printing the histogram values.
+        Follow file I/O as per usual to read the file
+    Tests could check to see if the function(s) correctly display the values in descending order and are not hard-coded
+        May need to split up functions to make them easier to test or to optimize structure(s)
+ */
