f0363a675738617670a38aad0e45aac7c4b41dde
MohitBallikar <mohitballikar@gmail.com>
2021-10-17 18:29:15 -0400

    uml + beginning of test

---
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -0,0 +1,14 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution42Test {
+    @Test
+    void testiftrue(){
+        Solution42 app = new Solution42();
+
+    }
+
+}
